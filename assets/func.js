e38e598805999b71cd91b5c1489c0c997ed879c4
naza-tech <ocbmf17@gmail.com>
2023-05-04 16:41:29 +0100

    i did the user dash bord
    today

---
+++ assets/func.js
@@ -0,0 +1,4 @@
+document.getElementsByClassName('line')[0].addEventListener("click",()=>{
+    document.getElementsByClassName('nav')[0].classList.toggle("nav-out")
+    document.getElementsByClassName('line')[0].classList.toggle("line-out")
+})
