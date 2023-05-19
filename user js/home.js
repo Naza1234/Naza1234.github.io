761b33dfd0bd30a7be0143ccc0fce4a3cb26587f
naza-tech <ocbmf17@gmail.com>
2023-05-19 19:06:32 +0100

    done
    done

--- user js/home.js
+++ user js/home.js
@@ -2,11 +2,28 @@ const url="https://invest-sevrer.onrender.com"
 const redURL="https://nazatech.me"
 
 let userID=localStorage.getItem("userID")
-
+var UserName
 document.getElementsByClassName('popup')[0].classList.add('popup-hid')
+document.getElementsByClassName('over')[0].classList.add('popup-hid')
 fetch(url + `/user/${userID}`)
 .then((res)=>res.json())
 .then((data)=>{
+    if(data.IDsent){
+        var html =`
+        <h1>
+        ${data.UserName}
+      </h1>
+      <p>
+        your ID is under verification by our admins. Pleas check your email if any confirmation mail has been sent.
+      </p>
+        `
+        document.getElementsByClassName('idault')[0].innerHTML=html
+       if(data.IDalt){
+        document.getElementsByClassName('idault')[0].classList.add('hid')
+       }
+    }
+    UserName=data.UserName
+   document.getElementsByClassName('over')[0].classList.remove('popup-hid')
    document.getElementsByClassName('popup')[0].classList.remove('popup-hid')
    document.getElementsByClassName('cap')[0].textContent=data.UserName
    document.getElementsByClassName('money')[0].innerHTML=`
@@ -112,4 +129,69 @@ if (userID===null) {
 }
 
 
+var uplaodButton=document.getElementsByTagName('input')[0]
+var image=document.getElementsByClassName('imgup')[0]
+var imgname=document.getElementsByClassName('nameid')[0]
 
+ uplaodButton.onchange = ()=>{
+    document.getElementsByClassName('imgup')[0].classList.remove('error')
+     let reader= new FileReader()
+     reader.readAsDataURL(uplaodButton.files[0])
+     reader.onload = () => {
+        image.setAttribute("src",reader.result)
+     }
+    imgname.innerHTML=uplaodButton.files[0].name
+ }
+
+ function ver(){
+    var uplaodButton=document.getElementsByTagName('input')[0]
+    if (uplaodButton.files[0]===undefined) {
+        console.log("upload");
+        document.getElementsByClassName('imgup')[0].classList.add('error')
+    }else{
+        document.getElementsByClassName('over')[0].classList.add('popup-hid')
+        var file={
+            image:uplaodButton.files[0],
+            fileNmae:UserName + " " + "ID",
+            user_id:userID
+        }
+        const formData= new FormData()
+        formData.append('image',uplaodButton.files[0])
+        formData.append("fileNmae",UserName + " " + "ID")
+        formData.append("user_id", userID )
+        for(item of formData){
+            console.log(item[0],item[1]);
+        }
+        fetch(url + "/userid",{
+            method:'POST',
+            body:formData,
+        })
+        .then((res)=>res.json())
+        .then((data)=>{
+            var info ={
+                IDsent:true
+            }
+            fetch(url + `/user/${userID}`,{
+                method:'PUT',
+                body: JSON.stringify(info),
+                headers: {
+                    'Content-type':'application/json'
+                }
+            })
+            .then((res)=>res.json())
+            .then((data)=>{
+                var html =`
+                <h1>
+                ${data.UserName}
+              </h1>
+              <p>
+                your ID is under verification by our admins. Pleas check your email if any confirmation mail has been sent.
+              </p>
+                `
+                document.getElementsByClassName('idault')[0].innerHTML=html
+                document.getElementsByClassName('over')[0].classList.remove('popup-hid')
+            })
+            console.log(data)
+        })
+    }
+ }
